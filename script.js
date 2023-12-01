8d02e590852dd3e61f216b758b4e642f2976b7d4
Amey Shinde <119589415+AmeyShinde9872@users.noreply.github.com>
2023-12-01 20:52:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 
-openBtn = document.querySelector(".openbtn");
+let openBtn = document.querySelector(".openbtn");
 openBtn.addEventListener("click",openNav);
 
 function openNav(){
     document.getElementById("myNav").style.width = "100%"
 }
 
-closeBtn = document.querySelector(".closebtn");
+let closeBtn = document.querySelector(".closebtn");
 closeBtn.addEventListener("click",closeNav);
 
 function closeNav(){
